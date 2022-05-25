a635ec0479e36afc14664a3f85ab95170259436c
Adrian <adrianwojciak@interia.pl>
2022-05-25 20:55:21 +0200

    debug log

--- src/incentive_apy.ts
+++ src/incentive_apy.ts
@@ -180,6 +180,8 @@ export const createSnapshotForNetwork = async (network: Network) => {
             currentTickIndexes?.[incentive.pool.toString()] ?? 0,
         };
 
+        console.log(incentiveApy);
+
         apy[incentive.publicKey.toString()] = {
           apy: isNaN(+JSON.stringify(incentiveApy.reward))
             ? 0
