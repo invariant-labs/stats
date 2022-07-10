44e9e17722cfe27bb5d668122c5170ebb8238e59
Adrian Wójciak <adrianwojciak@interia.pl>
2022-07-10 21:26:34 +0200

    Update incentive_apy.ts

--- src/incentive_apy.ts
+++ src/incentive_apy.ts
@@ -32,7 +32,7 @@ export const createSnapshotForNetwork = async (network: Network) => {
 
   switch (network) {
     case Network.MAIN:
-      provider = Provider.local("https://ssc-dao.genesysgo.net");
+      provider = Provider.local("https://rpc.nightly.app:8899/");
       fileName = "./data/incentive_apy_mainnet.json";
       ticksFolder = "./data/ticks/mainnet/";
       rewardsData = MAINNET_REWARDS;
