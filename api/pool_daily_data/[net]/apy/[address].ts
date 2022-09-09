aed4b532989124c23cd7cd2a1a9d5991ca1469e4
Adrian <adrianwojciak99@gmail.com>
2022-09-09 14:39:46 +0200

    should work

---
+++ api/pool_daily_data/[net]/apy/[address].ts
@@ -0,0 +1,45 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import DEVNET_APY from "../../../../data/pool_apy_archive_devnet.json";
+import MAINNET_APY from "../../../../data/pool_apy_archive_mainnet.json";
+import { PoolApyArchiveSnapshot } from "../../../../src/utils";
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  // @ts-expect-error
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  const { net, address, limit = "10", skip = "0" } = req.query;
+
+  let apyArchive: Record<string, PoolApyArchiveSnapshot[]>;
+
+  if (net === "devnet") {
+    apyArchive = DEVNET_APY;
+  } else if (net === "mainnet") {
+    apyArchive = MAINNET_APY;
+  } else {
+    res.status(400).send("INVALID NETWORK");
+    return;
+  }
+
+  const data = apyArchive?.[address as string] ?? [];
+
+  const formattedData = data.map((snap) => ({
+    date: snap.timestamp,
+    apy: snap.apy,
+    tradingLowerTick: snap.range.tickLower,
+    tradingUpperTick: snap.range.tickUpper,
+  }))
+
+  res.json(
+    formattedData.slice(-(Number(limit) + Number(skip))).slice(Number(limit))
+  );
+}
---
+++ api/pool_daily_data/[net]/stats/[address].ts
@@ -0,0 +1,130 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import BN from "bn.js";
+import DEVNET_DATA from "../../../../data/devnet.json";
+import MAINNET_DATA from "../../../../data/mainnet.json";
+import { PoolSnapshot, PoolStatsData, printBN } from "../../../../src/utils";
+
+const onlySnaps = (
+  data: Record<string, PoolStatsData>
+): Record<string, PoolSnapshot[]> => {
+  const newData: Record<string, PoolSnapshot[]> = {};
+
+  Object.entries(data).forEach(([address, pool]) => {
+    newData[address] = pool.snapshots;
+  });
+
+  return newData;
+};
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  // @ts-expect-error
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  const { net, address, limit = "10", skip = "0" } = req.query;
+
+  let data: Record<string, PoolStatsData>;
+
+  if (net === "devnet") {
+    data = DEVNET_DATA;
+  } else if (net === "mainnet") {
+    data = MAINNET_DATA;
+  } else {
+    res.status(400).send("INVALID NETWORK");
+    return;
+  }
+
+  const addressData = data?.[address as string];
+
+  if (typeof addressData === "undefined") {
+    res.json([]);
+    return;
+  }
+
+  const formattedData = addressData.snapshots.map((snap, index) => {
+    const prevData =
+      index > 0
+        ? addressData.snapshots[index - 1]
+        : {
+            volumeX: {
+              tokenBNFromBeginning: "0",
+            },
+            volumeY: {
+              tokenBNFromBeginning: "0",
+            },
+            liquidityX: {
+              tokenBNFromBeginning: "0",
+            },
+            liquidityY: {
+              tokenBNFromBeginning: "0",
+            },
+            feeX: {
+              tokenBNFromBeginning: "0",
+            },
+            feeY: {
+              tokenBNFromBeginning: "0",
+            },
+          };
+    const volumeX = +printBN(
+      new BN(snap.volumeX.tokenBNFromBeginning).sub(
+        new BN(prevData.volumeX.tokenBNFromBeginning)
+      ),
+      addressData.tokenX.decimals
+    );
+    const volumeY = +printBN(
+      new BN(snap.volumeY.tokenBNFromBeginning).sub(
+        new BN(prevData.volumeY.tokenBNFromBeginning)
+      ),
+      addressData.tokenY.decimals
+    );
+    const liquidityX = +printBN(
+      new BN(snap.liquidityX.tokenBNFromBeginning).sub(
+        new BN(prevData.liquidityX.tokenBNFromBeginning)
+      ),
+      addressData.tokenX.decimals
+    );
+    const liquidityY = +printBN(
+      new BN(snap.liquidityY.tokenBNFromBeginning).sub(
+        new BN(prevData.liquidityY.tokenBNFromBeginning)
+      ),
+      addressData.tokenY.decimals
+    );
+    const feeX = +printBN(
+      new BN(snap.feeX.tokenBNFromBeginning).sub(
+        new BN(prevData.feeX.tokenBNFromBeginning)
+      ),
+      addressData.tokenX.decimals
+    );
+    const feeY = +printBN(
+      new BN(snap.feeY.tokenBNFromBeginning).sub(
+        new BN(prevData.feeY.tokenBNFromBeginning)
+      ),
+      addressData.tokenY.decimals
+    );
+    return {
+      date: snap.timestamp,
+      volumeUsd: snap.volumeX.usdValue24 + snap.volumeY.usdValue24,
+      liquidityUsd: snap.liquidityX.usdValue24 + snap.liquidityY.usdValue24,
+      feeUsd: snap.feeX.usdValue24 + snap.feeY.usdValue24,
+      volumeX,
+      volumeY,
+      liquidityX,
+      liquidityY,
+      feeX,
+      feeY,
+    };
+  });
+
+  res.json(
+    formattedData.slice(-(Number(limit) + Number(skip))).slice(Number(limit))
+  );
+}
