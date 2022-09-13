eb6f6a6e462a1aff54caa438beca1b87406a424c
Adrian <adrianwojciak99@gmail.com>
2022-09-13 13:06:36 +0200

    bump sdk

--- src/calcTotalMainnetVolume.ts
+++ src/calcTotalMainnetVolume.ts
@@ -2,8 +2,8 @@ import MAINNET_DATA from "../data/mainnet.json";
 
 let sum = 0;
 
-Object.values(MAINNET_DATA).forEach((snaps) => {
-  snaps.forEach((snap) => {
+Object.values(MAINNET_DATA).forEach((data) => {
+  data.snapshots.forEach((snap) => {
     sum += snap.volumeX.usdValue24;
     sum += snap.volumeY.usdValue24;
   });
