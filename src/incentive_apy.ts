3a69ed6e6ed74de3eb841e44e3094d7b5e8f82e9
awojciak <adrianwojciak99@gmail.com>
2023-04-17 13:32:42 +0200

    fix snaps

--- src/incentive_apy.ts
+++ src/incentive_apy.ts
@@ -1,12 +1,12 @@
-import { Network, Market, getMarketAddress, Pair } from "@invariant-labs/sdk";
-import { PoolStructure } from "@invariant-labs/sdk/lib/market";
-import { rewardsAPY } from "@invariant-labs/sdk/lib/utils";
-import { Staker } from "@invariant-labs/staker-sdk";
-import { BN, Provider } from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js";
-import fs from "fs";
-import DEVNET_REWARDS from "../data/rewards_data_devnet.json";
-import MAINNET_REWARDS from "../data/rewards_data_mainnet.json";
+import { Network, Market, getMarketAddress, Pair } from '@invariant-labs/sdk'
+import { PoolStructure } from '@invariant-labs/sdk/lib/market'
+import { rewardsAPY } from '@invariant-labs/sdk/lib/utils'
+import { Staker } from '@invariant-labs/staker-sdk'
+import { BN, Provider } from '@project-serum/anchor'
+import { PublicKey } from '@solana/web3.js'
+import fs from 'fs'
+import DEVNET_REWARDS from '../data/rewards_data_devnet.json'
+import MAINNET_REWARDS from '../data/rewards_data_mainnet.json'
 import {
   marketToStakerNetwork,
   devnetTokensData,
@@ -15,160 +15,144 @@ import {
   getTokensPrices,
   RewardsData,
   IncentiveApySnapshot,
-} from "./utils";
+} from './utils'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
+require('dotenv').config()
 
 export const createSnapshotForNetwork = async (network: Network) => {
-  let provider: Provider;
-  let fileName: string;
-  let ticksFolder: string;
-  let rewardsData: Record<string, RewardsData>;
-  let tokensData: Record<string, TokenData>;
+  let provider: Provider
+  let fileName: string
+  let ticksFolder: string
+  let rewardsData: Record<string, RewardsData>
+  let tokensData: Record<string, TokenData>
 
   switch (network) {
     case Network.MAIN:
       provider = Provider.local(
-        "https://rpc.ankr.com/solana"
-      );
-      fileName = "./data/incentive_apy_mainnet.json";
-      ticksFolder = "./data/ticks/mainnet/";
-      rewardsData = MAINNET_REWARDS;
-      tokensData = await getTokensData();
-      break;
+        'https://081a13gougoozkk51v1ekgzu5jsjrcidg1ynuxpng4hstiospypw9str4gzu8.xyz2.hyperplane.dev/'
+      )
+      fileName = './data/incentive_apy_mainnet.json'
+      ticksFolder = './data/ticks/mainnet/'
+      rewardsData = MAINNET_REWARDS
+      tokensData = await getTokensData()
+      break
     case Network.DEV:
     default:
-      provider = Provider.local("https://api.devnet.solana.com");
-      fileName = "./data/incentive_apy_devnet.json";
-      ticksFolder = "./data/ticks/devnet/";
-      rewardsData = DEVNET_REWARDS;
-      tokensData = devnetTokensData;
+      provider = Provider.local('https://api.devnet.solana.com')
+      fileName = './data/incentive_apy_devnet.json'
+      ticksFolder = './data/ticks/devnet/'
+      rewardsData = DEVNET_REWARDS
+      tokensData = devnetTokensData
   }
 
-  const idsList: string[] = [];
+  const idsList: string[] = []
 
   Object.values(tokensData).forEach((token) => {
-    if (typeof token?.coingeckoId !== "undefined") {
-      idsList.push(token.coingeckoId);
+    if (typeof token?.coingeckoId !== 'undefined') {
+      idsList.push(token.coingeckoId)
     }
-  });
+  })
 
-  const coingeckoPrices = await getTokensPrices(idsList);
+  const coingeckoPrices = await getTokensPrices(idsList)
 
-  const connection = provider.connection;
+  const connection = provider.connection
 
   const market = await Market.build(
     network,
     provider.wallet,
     connection,
     new PublicKey(getMarketAddress(network))
-  );
+  )
 
-  const staker = await Staker.build(
-    marketToStakerNetwork(network),
-    provider.wallet,
-    connection
-  );
+  const staker = await Staker.build(marketToStakerNetwork(network), provider.wallet, connection)
 
-  const allPools = await market.getAllPools();
-  const allIncentives = await staker.getAllIncentive();
+  const allPools = await market.getAllPools()
+  const allIncentives = await staker.getAllIncentive()
 
-  const xPrices: Record<string, number> = {};
-  const apy: Record<string, IncentiveApySnapshot> = {};
-  const poolsData: Record<string, PoolStructure> = {};
-  const allLiquidity: Record<string, BN> = {};
+  const xPrices: Record<string, number> = {}
+  const apy: Record<string, IncentiveApySnapshot> = {}
+  const poolsData: Record<string, PoolStructure> = {}
+  const allLiquidity: Record<string, BN> = {}
 
-  await Promise.all(
+  await Promise.allSettled(
     allPools.map(async (pool) => {
-      const pair = new Pair(pool.tokenX, pool.tokenY, { fee: pool.fee.v });
-      const address = await pair.getAddress(market.program.programId);
+      const pair = new Pair(pool.tokenX, pool.tokenY, { fee: pool.fee.v })
+      const address = await pair.getAddress(market.program.programId)
       const tokenXData = tokensData?.[pool.tokenX.toString()] ?? {
         decimals: 0,
-      };
-      const tokenXPrice = tokenXData.coingeckoId
-        ? coingeckoPrices[tokenXData.coingeckoId] ?? 0
-        : 0;
+      }
+      const tokenXPrice = tokenXData.coingeckoId ? coingeckoPrices[tokenXData.coingeckoId] ?? 0 : 0
 
-      xPrices[address.toString()] = tokenXPrice;
-      poolsData[address.toString()] = pool;
-      allLiquidity[address.toString()] =
-        await market.getAllPoolLiquidityInTokens(address);
+      xPrices[address.toString()] = tokenXPrice
+      poolsData[address.toString()] = pool
+      allLiquidity[address.toString()] = await market.getAllPoolLiquidityInTokens(address)
     })
-  );
+  )
 
-  await Promise.all(
+  await Promise.allSettled(
     allIncentives.map(async (incentive) => {
-      const incentiveRewardData = rewardsData?.[incentive.publicKey.toString()];
+      const incentiveRewardData = rewardsData?.[incentive.publicKey.toString()]
       const rewardToken =
-        typeof incentiveRewardData === "undefined"
+        typeof incentiveRewardData === 'undefined'
           ? { decimals: 0 }
-          : tokensData?.[incentiveRewardData.token] ?? { decimals: 0 };
+          : tokensData?.[incentiveRewardData.token] ?? { decimals: 0 }
       const rewardTokenPrice = rewardToken.coingeckoId
         ? coingeckoPrices[rewardToken.coingeckoId] ?? 0
-        : 0;
+        : 0
 
       try {
         const incentiveApy = rewardsAPY({
           rewardInUsd:
-            typeof incentiveRewardData === "undefined"
+            typeof incentiveRewardData === 'undefined'
               ? 0
               : rewardTokenPrice * incentiveRewardData.total,
           tokenPrice: xPrices?.[incentive.pool.toString()] ?? 0,
           duration:
-            (incentive.endTime.v.toNumber() -
-              incentive.startTime.v.toNumber()) /
-            60 /
-            60 /
-            24,
+            (incentive.endTime.v.toNumber() - incentive.startTime.v.toNumber()) / 60 / 60 / 24,
           tokenDecimal: rewardToken.decimals,
-          currentTickIndex:
-            poolsData?.[incentive.pool.toString()]?.currentTickIndex ?? 0,
-          currentLiquidity:
-            poolsData?.[incentive.pool.toString()]?.liquidity?.v ?? new BN(0),
+          currentTickIndex: poolsData?.[incentive.pool.toString()]?.currentTickIndex ?? 0,
+          currentLiquidity: poolsData?.[incentive.pool.toString()]?.liquidity?.v ?? new BN(0),
           tickSpacing: poolsData?.[incentive.pool.toString()]?.tickSpacing ?? 1,
-          allLiquidityInTokens:
-            allLiquidity?.[incentive.pool.toString()] ?? new BN(0),
-        });
+          allLiquidityInTokens: allLiquidity?.[incentive.pool.toString()] ?? new BN(0),
+        })
 
         apy[incentive.publicKey.toString()] = {
-          apy: isNaN(+JSON.stringify(incentiveApy.apy))
-            ? 0
-            : incentiveApy.apy * 100,
+          apy: isNaN(+JSON.stringify(incentiveApy.apy)) ? 0 : incentiveApy.apy * 100,
           apySingleTick: isNaN(+JSON.stringify(incentiveApy.apySingleTick))
             ? 0
             : incentiveApy.apySingleTick * 100,
-        };
+        }
       } catch (_error) {
         apy[incentive.publicKey.toString()] = {
           apy: 0,
           apySingleTick: 0,
-        };
+        }
       }
     })
-  );
+  )
 
   fs.writeFile(fileName, JSON.stringify(apy), (err) => {
     if (err) {
-      throw err;
+      throw err
     }
-  });
-};
+  })
+}
 
 createSnapshotForNetwork(Network.DEV).then(
   () => {
-    console.log("Devnet incentive apy snapshot done!");
+    console.log('Devnet incentive apy snapshot done!')
   },
   (err) => {
-    console.log(err);
+    console.log(err)
   }
-);
+)
 
 createSnapshotForNetwork(Network.MAIN).then(
   () => {
-    console.log("Mainnet incentive apy snapshot done!");
+    console.log('Mainnet incentive apy snapshot done!')
   },
   (err) => {
-    console.log(err);
+    console.log(err)
   }
-);
+)
--- src/snap.ts
+++ src/snap.ts
@@ -1,10 +1,10 @@
-import { Network, Market, Pair, getMarketAddress } from "@invariant-labs/sdk";
-import { PoolStructure } from "@invariant-labs/sdk/lib/market";
-import { BN, Provider } from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js";
-import fs from "fs";
-import DEVNET_DATA from "../data/devnet.json";
-import MAINNET_DATA from "../data/mainnet.json";
+import { Network, Market, Pair, getMarketAddress } from '@invariant-labs/sdk'
+import { PoolStructure } from '@invariant-labs/sdk/lib/market'
+import { BN, Provider } from '@project-serum/anchor'
+import { PublicKey } from '@solana/web3.js'
+import fs from 'fs'
+import DEVNET_DATA from '../data/devnet.json'
+import MAINNET_DATA from '../data/mainnet.json'
 import {
   devnetTokensData,
   getTokensData,
@@ -13,91 +13,83 @@ import {
   PoolSnapshot,
   PoolStatsData,
   TokenData,
-} from "./utils";
+} from './utils'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
+require('dotenv').config()
 
 export const createSnapshotForNetwork = async (network: Network) => {
-  let provider: Provider;
-  let fileName: string;
-  let snaps: Record<string, PoolStatsData>;
-  let tokensData: Record<string, TokenData>;
+  let provider: Provider
+  let fileName: string
+  let snaps: Record<string, PoolStatsData>
+  let tokensData: Record<string, TokenData>
 
   switch (network) {
     case Network.MAIN:
       provider = Provider.local(
-        "https://rpc.ankr.com/solana"
-      );
-      fileName = "./data/mainnet.json";
-      snaps = MAINNET_DATA;
-      tokensData = await getTokensData();
-      break;
+        'https://081a13gougoozkk51v1ekgzu5jsjrcidg1ynuxpng4hstiospypw9str4gzu8.xyz2.hyperplane.dev/'
+      )
+      fileName = './data/mainnet.json'
+      snaps = MAINNET_DATA
+      tokensData = await getTokensData()
+      break
     case Network.DEV:
     default:
-      provider = Provider.local("https://api.devnet.solana.com");
-      fileName = "./data/devnet.json";
-      snaps = DEVNET_DATA;
-      tokensData = devnetTokensData;
+      provider = Provider.local('https://api.devnet.solana.com')
+      fileName = './data/devnet.json'
+      snaps = DEVNET_DATA
+      tokensData = devnetTokensData
   }
 
-  const idsList: string[] = [];
+  const idsList: string[] = []
 
   Object.values(tokensData).forEach((token) => {
-    if (typeof token?.coingeckoId !== "undefined") {
-      idsList.push(token.coingeckoId);
+    if (typeof token?.coingeckoId !== 'undefined') {
+      idsList.push(token.coingeckoId)
     }
-  });
+  })
 
-  const coingeckoPrices = await getTokensPrices(idsList);
+  const coingeckoPrices = await getTokensPrices(idsList)
 
-  const connection = provider.connection;
+  const connection = provider.connection
 
   const market = await Market.build(
     network,
     provider.wallet,
     connection,
     new PublicKey(getMarketAddress(network))
-  );
+  )
 
-  const allPools = await market.getAllPools();
+  const allPools = await market.getAllPools()
 
-  const poolsDict: Record<string, PoolStructure> = {};
+  const poolsDict: Record<string, PoolStructure> = {}
 
-  const poolsData = await Promise.all(
+  const poolsData = await Promise.allSettled(
     allPools.map(async (pool) => {
-      const pair = new Pair(pool.tokenX, pool.tokenY, { fee: pool.fee.v });
-      const address = await pair.getAddress(market.program.programId);
+      const pair = new Pair(pool.tokenX, pool.tokenY, { fee: pool.fee.v })
+      const address = await pair.getAddress(market.program.programId)
 
-      poolsDict[address.toString()] = pool;
+      poolsDict[address.toString()] = pool
 
-      const { volumeX, volumeY } = await market.getVolume(pair);
+      const { volumeX, volumeY } = await market.getVolume(pair)
 
-      const { liquidityX, liquidityY } = await market.getPairLiquidityValues(
-        pair
-      );
+      const { liquidityX, liquidityY } = await market.getPairLiquidityValues(pair)
 
-      const { feeX, feeY } = await market.getGlobalFee(pair);
+      const { feeX, feeY } = await market.getGlobalFee(pair)
 
-      let lastSnapshot: PoolSnapshot | undefined;
+      let lastSnapshot: PoolSnapshot | undefined
       const tokenXData = tokensData?.[pool.tokenX.toString()] ?? {
         decimals: 0,
-      };
+      }
       const tokenYData = tokensData?.[pool.tokenY.toString()] ?? {
         decimals: 0,
-      };
-      const tokenXPrice = tokenXData.coingeckoId
-        ? coingeckoPrices[tokenXData.coingeckoId] ?? 0
-        : 0;
-      const tokenYPrice = tokenYData.coingeckoId
-        ? coingeckoPrices[tokenYData.coingeckoId] ?? 0
-        : 0;
+      }
+      const tokenXPrice = tokenXData.coingeckoId ? coingeckoPrices[tokenXData.coingeckoId] ?? 0 : 0
+      const tokenYPrice = tokenYData.coingeckoId ? coingeckoPrices[tokenYData.coingeckoId] ?? 0 : 0
 
       if (snaps?.[address.toString()]) {
         lastSnapshot =
-          snaps[address.toString()].snapshots[
-            snaps[address.toString()].snapshots.length - 1
-          ];
+          snaps[address.toString()].snapshots[snaps[address.toString()].snapshots.length - 1]
       }
 
       return {
@@ -109,7 +101,7 @@ export const createSnapshotForNetwork = async (network: Network) => {
               volumeX,
               tokenXData.decimals,
               tokenXPrice,
-              typeof lastSnapshot !== "undefined"
+              typeof lastSnapshot !== 'undefined'
                 ? new BN(lastSnapshot.volumeX.tokenBNFromBeginning)
                 : new BN(0)
             ),
@@ -120,28 +112,18 @@ export const createSnapshotForNetwork = async (network: Network) => {
               volumeY,
               tokenYData.decimals,
               tokenYPrice,
-              typeof lastSnapshot !== "undefined"
+              typeof lastSnapshot !== 'undefined'
                 ? new BN(lastSnapshot.volumeY.tokenBNFromBeginning)
                 : new BN(0)
             ),
           },
           liquidityX: {
             tokenBNFromBeginning: liquidityX.toString(),
-            usdValue24: getUsdValue24(
-              liquidityX,
-              tokenXData.decimals,
-              tokenXPrice,
-              new BN(0)
-            ),
+            usdValue24: getUsdValue24(liquidityX, tokenXData.decimals, tokenXPrice, new BN(0)),
           },
           liquidityY: {
             tokenBNFromBeginning: liquidityY.toString(),
-            usdValue24: getUsdValue24(
-              liquidityY,
-              tokenYData.decimals,
-              tokenYPrice,
-              new BN(0)
-            ),
+            usdValue24: getUsdValue24(liquidityY, tokenYData.decimals, tokenYPrice, new BN(0)),
           },
           feeX: {
             tokenBNFromBeginning: feeX.toString(),
@@ -149,7 +131,7 @@ export const createSnapshotForNetwork = async (network: Network) => {
               feeX,
               tokenXData.decimals,
               tokenXPrice,
-              typeof lastSnapshot !== "undefined"
+              typeof lastSnapshot !== 'undefined'
                 ? new BN(lastSnapshot.feeX.tokenBNFromBeginning)
                 : new BN(0)
             ),
@@ -160,66 +142,68 @@ export const createSnapshotForNetwork = async (network: Network) => {
               feeY,
               tokenYData.decimals,
               tokenYPrice,
-              typeof lastSnapshot !== "undefined"
+              typeof lastSnapshot !== 'undefined'
                 ? new BN(lastSnapshot.feeY.tokenBNFromBeginning)
                 : new BN(0)
             ),
           },
         },
-      };
+      }
     })
-  );
+  )
 
-  const now = Date.now();
+  const now = Date.now()
   const timestamp =
-    Math.floor(now / (1000 * 60 * 60 * 24)) * (1000 * 60 * 60 * 24) +
-    1000 * 60 * 60 * 12;
+    Math.floor(now / (1000 * 60 * 60 * 24)) * (1000 * 60 * 60 * 24) + 1000 * 60 * 60 * 12
 
-  poolsData.forEach(({ address, stats }) => {
-    const xAddress = poolsDict[address].tokenX.toString();
-    const yAddress = poolsDict[address].tokenY.toString();
+  poolsData
+    .filter((data) => data.status !== 'rejected')
+    .forEach((data) => {
+      const { address, stats } = (data as PromiseFulfilledResult<any>).value
+      const xAddress = poolsDict[address].tokenX.toString()
+      const yAddress = poolsDict[address].tokenY.toString()
 
-    if (!snaps[address]) {
-      snaps[address] = {
-        snapshots: [],
-        tokenX: {
-          address: xAddress,
-          decimals: tokensData?.[xAddress]?.decimals ?? 0,
-        },
-        tokenY: {
-          address: yAddress,
-          decimals: tokensData?.[yAddress]?.decimals ?? 0,
-        },
-      };
-    }
+      if (!snaps[address]) {
+        snaps[address] = {
+          snapshots: [],
+          tokenX: {
+            address: xAddress,
+            decimals: tokensData?.[xAddress]?.decimals ?? 0,
+          },
+          tokenY: {
+            address: yAddress,
+            decimals: tokensData?.[yAddress]?.decimals ?? 0,
+          },
+        }
+      }
 
-    snaps[address].snapshots.push({
-      timestamp,
-      ...stats,
-    });
-  });
+      snaps[address].snapshots.push({
+        timestamp,
+        ...stats,
+      })
+    })
 
   fs.writeFile(fileName, JSON.stringify(snaps), (err) => {
     if (err) {
-      throw err;
+      throw err
     }
-  });
-};
+  })
+}
 
 createSnapshotForNetwork(Network.DEV).then(
   () => {
-    console.log("Devnet snapshot done!");
+    console.log('Devnet snapshot done!')
   },
   (err) => {
-    console.log(err);
+    console.log(err)
   }
-);
+)
 
 createSnapshotForNetwork(Network.MAIN).then(
   () => {
-    console.log("Mainnet snapshot done!");
+    console.log('Mainnet snapshot done!')
   },
   (err) => {
-    console.log(err);
+    console.log(err)
   }
-);
+)
