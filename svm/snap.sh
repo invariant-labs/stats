89633c20122d60d6d60955c21b12815b64e93959
Wojciech <cichockiwojciech97@gmail.com>
2024-11-01 11:34:58 +0100

    Update snap

--- svm/snap.sh
+++ svm/snap.sh
@@ -1,11 +1,11 @@
 export NODE_OPTIONS="--max-old-space-size=8192"
 
-# ts-node ./src/snap.ts
-# ts-node ./src/ticks.ts
-# ts-node ./src/pool_apy.ts
-# ts-node ./src/full-snap.ts
+ts-node ./src/snap.ts
+ts-node ./src/ticks.ts
+ts-node ./src/pool_apy.ts
+ts-node ./src/full-snap.ts
 
-ts-node ./src/eclipse/snap.ts
+# ts-node ./src/eclipse/snap.ts
 # ts-node ./src/eclipse/ticks.ts
 # ts-node ./src/eclipse/pool_apy.ts
-ts-node ./src/eclipse/full-snap.ts
+# ts-node ./src/eclipse/full-snap.ts
