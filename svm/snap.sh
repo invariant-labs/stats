513f32031e6f2460ca06be5b85e5244a45936a36
Wojciech <cichockiwojciech97@gmail.com>
2024-11-15 12:26:35 +0100

    add manual snap sol

--- svm/snap.sh
+++ svm/snap.sh
@@ -5,7 +5,7 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 # ts-node ./src/pool_apy.ts
 # ts-node ./src/full-snap.ts
 
-# ts-node ./src/eclipse/snap.ts
+ts-node ./src/eclipse/snap.ts
 # ts-node ./src/eclipse/ticks.ts
 # ts-node ./src/eclipse/pool_apy.ts
-# ts-node ./src/eclipse/full-snap.ts
+ts-node ./src/eclipse/full-snap.ts
