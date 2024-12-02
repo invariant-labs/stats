bc9980a79d065ec9a29e7d395dae87e2ca2101f3
Sniezka <dygons14@gmail.com>
2024-12-02 12:02:59 +0100

    Uncomment mainnet

--- eclipse/snap.sh
+++ eclipse/snap.sh
@@ -3,4 +3,4 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 npx tsx ./src/snap.ts
 # npx tsx ./src/ticks.ts
 # npx tsx ./src/pool_apy.ts
-npx tsx ./src/full-snap.ts
+# npx tsx ./src/full-snap.ts
