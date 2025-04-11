ae8e4a37ddbf46669488b62747e4445da79f6275
Aleksander <dygons14@gmail.com>
2025-04-11 12:58:59 +0200

    bump

--- solana/snap.sh
+++ solana/snap.sh
@@ -3,7 +3,7 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 
 RPC_URL="$1"
 
-ts-node ./src/snap.ts "$RPC_URL"
-ts-node ./src/ticks.ts "$RPC_URL"
-ts-node ./src/pool_apy.ts "$RPC_URL"
-ts-node ./src/full-snap.ts "$RPC_URL"
+npx tsx ./src/snap.ts "$RPC_URL"
+npx tsx ./src/ticks.ts "$RPC_URL"
+npx tsx ./src/pool_apy.ts "$RPC_URL"
+npx tsx ./src/full-snap.ts "$RPC_URL"
