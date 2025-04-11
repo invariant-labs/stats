b202e7e018c16ee82e99030bf3cba532afb24e17
Aleksander <dygons14@gmail.com>
2025-04-11 13:52:10 +0200

    Add debugging echos

--- solana/snap.sh
+++ solana/snap.sh
@@ -3,6 +3,8 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 
 RPC_URL="$1"
 
+echo "RPC URL: $RPC_URL"
+
 npx tsx ./src/snap.ts "$RPC_URL"
 npx tsx ./src/ticks.ts "$RPC_URL"
 npx tsx ./src/pool_apy.ts "$RPC_URL"
