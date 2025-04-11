b5d7f181d5cf5aeaf1a22c8166e8e2e763af3b80
Aleksander <dygons14@gmail.com>
2025-04-11 09:50:37 +0200

    Add checks for definition

--- solana/snap.sh
+++ solana/snap.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 export NODE_OPTIONS="--max-old-space-size=8192"
 
+echo "SOLANA_RPC_URL is set: $([ -n "$SOLANA_RPC_URL" ] && echo 'Yes' || echo 'No')"
+
 ts-node ./src/snap.ts
-ts-node ./src/ticks.ts
-ts-node ./src/pool_apy.ts
-ts-node ./src/full-snap.ts
+# ts-node ./src/ticks.ts
+# ts-node ./src/pool_apy.ts
+# ts-node ./src/full-snap.ts
