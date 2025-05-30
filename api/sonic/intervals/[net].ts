7611e36f2817810067862c58653b163c15204fca
Aleksander <dygons14@gmail.com>
2025-05-30 17:52:42 +0200

    Dont use sonic package in api endpoint

--- api/sonic/intervals/[net].ts
+++ api/sonic/intervals/[net].ts
@@ -4,10 +4,10 @@ import SONIC_TESTNET_DATA from "../../../data/sonic/testnet_intervals.json";
 //@ts-ignore
 import SONIC_MAINNET_DATA from "../../../data/sonic/mainnet_intervals.json";
 
-import {
-  mapStringToInterval,
-  TotalIntervalStats,
-} from "../../../sonic/src/utils";
+// import {
+//   mapStringToInterval,
+//   TotalIntervalStats,
+// } from "../../../sonic/src/utils";
 
 export default function (req: VercelRequest, res: VercelResponse) {
   // @ts-expect-error
@@ -23,21 +23,22 @@ export default function (req: VercelRequest, res: VercelResponse) {
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
 
-  const { net, interval: rawInterval } = req.query;
+  const { net, interval } = req.query;
   let data;
   if (net === "sonic-mainnet") {
-    data = SONIC_MAINNET_DATA as TotalIntervalStats;
+    data = SONIC_MAINNET_DATA as any;
   } else if (net === "sonic-testnet") {
-    data = SONIC_TESTNET_DATA as TotalIntervalStats;
+    data = SONIC_TESTNET_DATA as any;
   } else {
+    throw new Error("Invalid network specified");
   }
 
-  const interval = mapStringToInterval(rawInterval as string);
+  // const interval = mapStringToInterval(rawInterval as string);
   const dailyData = data.daily;
   const volume24 = dailyData.volume;
   const tvl24 = dailyData.tvl;
   const fees24 = dailyData.fees;
-  const intervalData = data[interval];
+  const intervalData = data[interval as string];
   intervalData.volumePlot = intervalData.volumePlot.slice(0, 30);
   intervalData.liquidityPlot = intervalData.liquidityPlot.slice(0, 30);
 
