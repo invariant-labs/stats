df5f14fb9af509934afd78ddd4437c115c353c91
matepal00 <mateusz.paluch2311@gmail.com>
2025-05-30 14:14:51 +0200

    bump data

--- sonic/snap.sh
+++ sonic/snap.sh
@@ -3,5 +3,5 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 npx tsx ./src/snap.ts true
 # npx tsx ./src/ticks.ts
 npx tsx ./src/daily_pool_apy.ts true
-npx tsx ./src/aggregate-intervals.ts
+npx tsx ./src/aggregate-intervals.ts true
 npx tsx ./src/full-snap.ts true
