c1bae11a3bc8d48188632d3f52fe56b0bb53b2e9
Aleksander <dygons14@gmail.com>
2025-06-12 10:06:43 +0200

    Add endpoint for pool analytics

---
+++ api/eclipse/pools/[net].ts
@@ -0,0 +1,44 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+//@ts-ignore
+import TIMESTAMP from "../../../data/eclipse/timestamp.json";
+import fs from "fs";
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  // @ts-expect-error
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  const { net, interval, address } = req.query;
+
+  const network = (net as string).split("-")[1] as "mainnet" | "testnet";
+
+  const path = `../../../data/eclipse/intervals/${network}/${address}.json`;
+  const data = fs.existsSync(path)
+    ? JSON.parse(fs.readFileSync(path, "utf-8"))
+    : null;
+
+  if (!data) {
+    throw new Error(`Data not found for address: ${address}`);
+  }
+
+  const intervalData = data[interval as string];
+  intervalData.volumePlot = intervalData.volumePlot.slice(0, 30);
+  intervalData.liquidityPlot = intervalData.liquidityPlot.slice(0, 30);
+  intervalData.feesPlot = intervalData.feesPlot.slice(0, 30);
+
+  const response = {
+    timestamp: TIMESTAMP.v,
+    ...intervalData,
+  };
+
+  res.json(response);
+}
