bb4260c7e262189039cba51bec6467bf9c1337bd
Aleksander <dygons14@gmail.com>
2025-11-12 16:58:31 +0100

    Update specified pools

--- api/eclipse/preview.ts
+++ api/eclipse/preview.ts
@@ -1,9 +1,18 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import ECLIPSE_MAINNET_DATA from "../../data/eclipse/mainnet_intervals.json";
+import path from "path";
+import fs from "fs";
+import { calculateAPYForInterval } from "../../eclipse/src/utils";
+
+const pools = [
+  { address: "HRgVv1pyBLXdsAddq4ubSqo8xdQWRrYbvmXqEDtectce", fee: 900000000 },
+  { address: "FvVsbwsbGVo6PVfimkkPhpcRfBrRitiV946nMNNuz7f9", fee: 100000000 },
+  { address: "E2B7KUFwjxrsy9cC17hmadPsxWHD1NufZXTyrtuz8YxC", fee: 900000000 },
+  { address: "86vPh8ctgeQnnn8qPADy5BkzrqoH5XjMCWvkd4tYhhmM", fee: 900000000 },
+];
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  // const main = () => {
   // @ts-expect-error
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -41,6 +50,42 @@ export default function (req: VercelRequest, res: VercelResponse) {
   const tvl24 = get24hValue(liquidityPlot);
   const fees24 = get24hValue(feesPlot);
 
+  const poolsData = data.daily.poolsData;
+  const poolParams = (address: string, fee: number) => {
+    const filePath = path.join(
+      process.cwd(),
+      "data",
+      "eclipse",
+      "intervals",
+      "mainnet",
+      `${address}.json`
+    );
+
+    const data = fs.existsSync(filePath)
+      ? JSON.parse(fs.readFileSync(filePath, "utf-8"))
+      : null;
+    if (!data) {
+      throw new Error(`Data not found for address: ${address}`);
+    }
+
+    const volumePlot = data.daily.volumePlot.filter(predicate);
+    const liquidityPlot = data.daily.liquidityPlot.filter(predicate);
+    const feesPlot = data.daily.feesPlot.filter(predicate);
+    const volume = volumePlot[0]?.value || 0;
+    const tvl = liquidityPlot[0]?.value || 0;
+    // const fees = feesPlot[0]?.value || 0;
+    const apy = calculateAPYForInterval(volume, tvl, fee);
+    const index = poolsData.findIndex((item: any) => item.address === address);
+    poolsData[index] = {
+      ...poolsData[index],
+      volume,
+      tvl,
+      apy,
+    };
+  };
+
+  pools.forEach((pool) => poolParams(pool.address, pool.fee));
+
   const response = {
     timestamp: volumePlot[0]?.timestamp || 0,
     volume24,
@@ -54,7 +99,7 @@ export default function (req: VercelRequest, res: VercelResponse) {
     volumePlot: volumePlot.slice(0, 30),
     liquidityPlot: liquidityPlot.slice(0, 30),
     feesPlot: feesPlot.slice(0, 30),
-    poolsData: data.daily.poolsData,
+    poolsData,
     tokensData: data.daily.tokensData,
   };
 
